048218811ee052343ec1f9c13bf6ee80f292a420
vatsaldhamanwala <dhamanwalavatsal@gmail.com>
2023-03-28 09:49:03 +0530

    creating post using fast api

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello")
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+# request get method url get with: "/"
+
+
+@app.get("/")
+async def root():
+    return {"message": "hello world"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "this is your post"}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} content: {payload['content']}"}
